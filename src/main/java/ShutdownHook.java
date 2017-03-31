7077da66792fba69d00e487ab0ce453712210a29
znb <znbwo@qq.com>
2017-04-01 01:53:52 +0800

    test

--- src/main/java/ShutdownHook.java
+++ src/main/java/ShutdownHook.java
@@ -8,11 +8,11 @@ public class ShutdownHook implements Runnable {
     public void run() {
         System.out.println("ShutdownHook execute start...");
         System.out.print("Netty NioEventLoopGroup shutdownGracefully...");
-//        try {
-//            TimeUnit.SECONDS.sleep(10);//模拟应用进程退出前的处理操作
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            TimeUnit.SECONDS.sleep(10);//模拟应用进程退出前的处理操作
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         System.out.println("ShutdownHook execute end...");
         System.out.println("Sytem shutdown over, the cost time is 10000MS");
     }
